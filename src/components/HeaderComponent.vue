4fc4811d47bcdbd9bdfdb0fd1fbc827594083073
José Kahl <josekahl333@gmailcom>
2023-11-23 18:51:17 -0300

    feat: Criação das páginas Home e Gerador e criação da header
    Páginas Home e Generator criadas, conexão delas feita com o Vue Router e a header foi inicialiada. Ícone do site alterado.

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -3,10 +3,36 @@
 
 <template>
     <header>
-
+        <router-link :to="'/'">
+            <div class="logo">
+                <img src="../assets/img/logo.png">
+                <p>Nova Arte</p>
+            </div>
+        </router-link>
     </header>
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@300&display=swap');
 
+header {
+    display: flex;
+    height: 80px;
+    box-shadow: 0px -1px 10px 1px black;
+}
+
+.logo {
+    display: flex;
+    padding-left: 16px;
+}
+
+.logo > img {
+    width: 80px;
+}
+
+.logo > p {
+    margin: auto;
+    font-size:xx-large;
+    font-family: 'Inter', sans-serif;
+}
 </style>
