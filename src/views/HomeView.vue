a03dae4c2906045317ed98b9c4a773e1fb86e4cd
JSkahl <josekahl333@gmail.com>
2023-11-28 17:14:15 -0300

    update: Adição do botão na página Home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,9 +7,14 @@ import HeaderComponent from '../components/HeaderComponent.vue';
 <HeaderComponent />
 
 <main>
-    <div class="textos">
-        <h1>Artes inovadoras</h1>
-        <p>Descubra novas artes com o poder de uma API lotada com artes cobiçadas ao redor de todo o globo terrestre.</p>
+    <div class="introducao">
+        <p class="titulo">Artes inovadoras</p>
+        <p class="texto">Descubra novas artes com o poder de uma API lotada com artes cobiçadas ao redor de todo o globo terrestre.</p>
+        <div class="botao">
+            <button class="comecar" @click="$router.push('/generator')">
+            Comece agora!
+        </button>
+        </div>
     </div>
 
     <div class="ilustracao">
@@ -20,19 +25,23 @@ import HeaderComponent from '../components/HeaderComponent.vue';
 </template>
 
 <style>
+
 main {
     display: flex;
     flex-direction: row;
 }
 
-.textos {
+.introducao {
     margin: 16% 8% 0% 5%;
     width: 30%;
-    height: 30vh;
+    height: 100%;
 }
 
-.textos > h1 {
-    margin-bottom: 5%;
+.titulo {
+    font-size: 55px;
+    font-weight: bolder;
+    margin-bottom: 2%;
+    font-family: 'Inter', sans-serif;
 }
 
 .ilustracao {
@@ -42,5 +51,28 @@ main {
 .ilustracao > img {
     width: 50vw;
     height: 100vh;
+}
+
+.comecar {
+    width: 30vh;
+    height: 7vh;
+    margin-top: 10%;
+    font-size: x-large;
+    border: none;
+    background-color: #221900; 
+    color: white;
+    border-radius: 10px;
+}
+
+.comecar:hover {
+    border: 2px solid #221900;
+    background-color: rgb(233, 233, 233);
+    color: black;
+    transition: 200ms;
+}
+
+.botao {
+    display: flex;
+    justify-content: center;
 }
 </style>
